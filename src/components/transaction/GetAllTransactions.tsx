2e96612c24f078cb0ab37fbda840cef413c6f1b1
Bianca Lujan <bblanc545@aol.com>
2021-12-16 15:09:56 -0700

    fixed GetAllTransactions so that it returns transactions; no transactions found if not found

--- src/components/transaction/GetAllTransactions.tsx
+++ src/components/transaction/GetAllTransactions.tsx
@@ -4,32 +4,38 @@ const GetAllTransactions = () => {
   const [transactions, setTransactions] = useState<Array<TransactionProps>>()
   const [error, setError] = useState(false)
   const [errorMessage, setErrorMessage] = useState(null)
-
-  const getTransactions = () => {
-    fetch('https://nestjs-bank-app.herokuapp.com/transactions')
-      .then((res) => {
-        if (!res.ok) {
-          setError(true)
-          throw Error(res.statusText)
-        }
-        console.log('response', res.json())
-        return res.json()
-      })
-      .then((data) => setTransactions(data))
-      .catch((e) => setErrorMessage(e.message))
-  }
+  const [isFetchingData, setIsFetchingData] = useState(false)
+  const [isSuccessful, setIsSuccessful] = useState(false)
 
   return (
     <div className="GetAllTransactionsSection">
       <h2>View all transactions</h2>
-      <button className="getTransactionsButton" onClick={getTransactions}>
+      <button
+        className="getTransactionsButton"
+        onClick={() => {
+          setIsFetchingData(true)
+          fetch('https://nestjs-bank-app.herokuapp.com/transactions')
+            .then((res) => {
+              setIsFetchingData(false)
+              if (!res.ok) {
+                setIsSuccessful(false)
+                setError(true)
+                throw Error(res.statusText)
+              }
+              setIsSuccessful(true)
+              setError(false)
+              return res.json()
+            })
+            .then((data) => setTransactions(data))
+            .catch((e) => setErrorMessage(e.message))
+        }}
+      >
         Get transactions
       </button>
+      {isFetchingData ? <div className="loading">Please wait...</div> : null}
       {error ? <div>An error occurred: {errorMessage}</div> : null}
       <div>
-        {!transactions ? (
-          <h4>No transactions found</h4>
-        ) : (
+        {transactions && isSuccessful ? (
           <>
             {transactions?.map((transaction, i) => (
               <div key={i} className="singleTransaction">
@@ -43,7 +49,10 @@ const GetAllTransactions = () => {
               </div>
             ))}
           </>
-        )}
+        ) : null}
+        {transactions?.length === 0 && isSuccessful ? (
+          <h4>No transactions found</h4>
+        ) : null}
       </div>
     </div>
   )
