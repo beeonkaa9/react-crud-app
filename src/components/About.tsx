effd7f3498c13d8eaf2b5c7edc0135203c981037
Bianca Lujan <bblanc545@aol.com>
2021-12-29 16:45:17 -0700

    React Router v6 integration complete

---
+++ src/components/About.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+const About = () => {
+  return (
+    <div className="PageSection">
+      <h1 className="sectionHeader">About Us</h1>
+      <div className="aboutSection">
+        <h2>About U.S. National Bank</h2>
+        <p>U.S. National Bank was established in 1996 as a bank.</p>
+        <p>It is a very impressive bank that puts customers first.</p>
+        <p>U.S. National Bank is a bank with money.</p>
+        <p>We always put customers first and care about them.</p>
+        <p>It is the largest bank in the northern hemisphere.</p>
+        <p>U.S. National Bank is a bank that is in the United States.</p>
+      </div>
+
+      <div className="sectionContainer">
+        <h2>Contact Us</h2>
+        <h3>Via Phone</h3>
+        <p>1-800-333-5750</p>
+        <p>Monday through Friday</p>
+        <p>9:00 A.M. to 9:00 P.M. (MST)</p>
+        <h3>Via Email</h3>
+        <p>usnbanksupport@usnbank.com</p>
+      </div>
+    </div>
+  )
+}
+
+export default About
